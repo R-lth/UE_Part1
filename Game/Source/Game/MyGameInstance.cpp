6615b8c8968e229880bc493157c19636aed5a3a0
R-lth <bluearth0502@gmail.com>
2025-09-30 15:20:55 +0900

    :white_check_mark: GameInstanceClass를 MyGameInstance로 설정 후 로그 찍기

--- Game/Source/Game/MyGameInstance.cpp
+++ Game/Source/Game/MyGameInstance.cpp
@@ -1,5 +1,11 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "MyGameInstance.h"
 
+void UMyGameInstance::Init()
+{
+	Super::Init();
+	// Custom initialization code here
+	UE_LOG(LogTemp, Log, TEXT("%s"), TEXT("MyGameInstance Initialized"));
+}

f7726531c7ec4cba69da65cec85924162c0b3f62
R-lth <bluearth0502@gmail.com>
2025-09-30 16:38:54 +0900

    FName 실습. Pool

--- Game/Source/Game/MyGameInstance.cpp
+++ Game/Source/Game/MyGameInstance.cpp
@@ -54,5 +54,22 @@ void UMyGameInstance::Init()
         *FloatString,
         *IntString
     );
-#pragma endregion    
+#pragma endregion
+
+    // FName의 특징 살펴보기 (대소문자 구분없음)
+    FName Key1(TEXT("PELVIS"));
+	FName Key2(TEXT("pelvis"));
+
+    // 비교 결과 확인
+	FString Result = (Key1 == Key2) ? TEXT("Equal") : TEXT("Not Equal");
+	UE_LOG(LogTemp, Log, TEXT("FName Comparison: %s"), *Result);
+
+    // 값이 있는지 비교 (읽기 전용)
+    for (int i =0; i < 5; ++i)
+    {
+        // Name 풀에 검색하는 과정이 있기 때문에,
+        // 반복 횟수가 많은 경우네느 부하가 클 수 있음.
+        // static 키워드를 활용해서 부하를 줄이는 방법을 고려.
+        const static FName Key = FName(FName(TEXT("pelvis")));
+	}
 }

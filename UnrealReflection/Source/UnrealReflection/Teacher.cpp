e030e84e10fd013a4e8ee1b4ecfd158508f026e0
R-lth <bluearth0502@gmail.com>
2025-10-02 11:38:40 +0900

    Unreal Reflection

---
+++ UnrealReflection/Source/UnrealReflection/Teacher.cpp
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Teacher.h"
+
+UTeacher::UTeacher()
+{
+	Name = TEXT("장세윤");
+	Year = 14;
+	Id = 1;
+}
+
+void UTeacher::DoLesson()
+{
+	Super::DoLesson();
+
+	UE_LOG(LogTemp, Log, 
+		TEXT("%d년차 선생님 %s님이 수업을 강의합니다."), 
+		Year, *Name
+	);
+}
